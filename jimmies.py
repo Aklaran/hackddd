7d81ee9432bb236c7ab2ada7d0f74ab6639df098
James <jpupalai@andrew.cmu.edu>
2017-10-28 12:50:53 -0400

    basic visuals integrated
    sumamation done, boxes in place, adding text next

---
+++ jimmies.py
@@ -0,0 +1,109 @@
+###James' graphic part right now
+from tkinter import*
+
+def init(data):
+    # load data.xyz as appropriate
+    data.activated = False
+    
+   
+
+def mousePressed(event, data):
+    # use event.x and event.y
+    pass
+
+def keyPressed(event, data):
+    if event.keysym == "a":
+        data.activated = True
+    pass
+
+
+def timerFired(data):
+    pass
+
+def redrawAll(canvas, data):
+    if data.activated:
+        listmaker(4,5,4,canvas)
+    initialForm(4,5,canvas, data)
+    
+
+        
+        
+
+def listmaker(m,n,k, canvas):
+    l = []
+    i = 0
+    a = m
+    b = n
+    c = k
+    while i <= k:
+        l.append([a,b,i,k-i])
+        i+= 1
+        
+    
+def initialForm(m,n,canvas, data):
+    r = 2
+    s = data.width // 70
+    canvas.create_line(data.width/20  * .97, data.height/20 * r, data.width/10, data.height/20 * r, width = s//2)
+    canvas.create_line(data.width/20 , data.height/20 * r, data.width / 20 * 1.5 + 2, data.height/20 * (r + 1) + 2, width = s//2)
+    canvas.create_line(data.width / 20 * 1.5, data.height / 20 * (r + 1), data.width/20, data.height/20 * (r + 2), width = s//2)
+    canvas.create_line(data.width/20 * .97, data.height/20 * (r + 2), data.width/10, data.height/20 * (r + 2), width = s//2)
+    canvas.create_text(data.width/16  , data.height/20 * (3 * r / 4) * .95, text = "k", font = "helvetica "+ str(s), anchor = W)
+    canvas.create_text(data.width/16 , data.height/20 * ((21 * (r+1)) / 16 * 1.05 ), text = "i = 0", anchor = NW, font = "helvetica " + str(s))
+    canvas.create_text(data.width / 10 * (7 / 4),data.height/20 * (r + 1), text = "(  )", font = "helvetica " + str(4*s))
+    canvas.create_text(data.width / 10 * (11 / 4),data.height/20 * (r + 1), text = "(  )", font = "helvetica " + str(4*s))
+    canvas.create_text(data.width / 10 * 7/4, data.height/20 * (r + 1) * .9, text = str(n), font = "helvetica " + str(int(1.5 * s )))
+    canvas.create_text(data.width / 10 * 7/4, data.height/20 * (r + 1) * 1.25, text = "i", font = "helvetica " + str(int(1.5 * s)))
+    canvas.create_text(data.width / 10 * 2.8, data.height/20 * (r + 1) * .9, text = str(m), font = "helvetica " + str(int(1.3 * s )))
+    canvas.create_text(data.width / 10 * 2.8, data.height/20 * (r + 1) * 1.25, text = "k - i", font = "helvetica " + str(int(1.3 * s)))
+    canvas.create_line(data.width / 10 * 17/8, data.height / 20 * (r + 3), data.width / 10 * 17/8, data.height / 20 * (r + 5), width = int(s / 1.3))
+    canvas.create_polygon(data.width / 10 * 9/4, data.height / 20 * (r + 5), data.width / 10 * 2, data.height / 20 * (r + 5), data.width / 10 * 17/8, data.height / 20 * (r + 5), width = 5, fill = "red")
+    canvas.create_rectangle(data.width / 20, data.height / 20 * (r + 6), data.width/5, data.height/20 * (r + 11), width = 4)
+    canvas.create_line(data.width / 10 * 2.1, data.height / 20 * (r + 8) * .97, data.width / 10 * 2.3,  data.height / 20 * (r + 8) * .97, width = 2)
+    canvas.create_line(data.width / 10 * 2.1, data.height / 20 * (r + 8) * 1.03, data.width / 10 * 2.3,  data.height / 20 * (r + 8) * 1.03, width = 2)
+    
+    
+        
+        
+def run(width=300, height=300):
+    def redrawAllWrapper(canvas, data):
+        canvas.delete(ALL)
+        canvas.create_rectangle(0, 0, data.width, data.height,
+                                fill='white', width=0)
+        redrawAll(canvas, data)
+        canvas.update()    
+
+    def mousePressedWrapper(event, canvas, data):
+        mousePressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def keyPressedWrapper(event, canvas, data):
+        keyPressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def timerFiredWrapper(canvas, data):
+        timerFired(data)
+        redrawAllWrapper(canvas, data)
+        # pause, then call timerFired again
+        canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+    # Set up data and call init
+    class Struct(object): pass
+    data = Struct()
+    data.width = width
+    data.height = height
+    data.timerDelay = 100 # milliseconds
+    init(data)
+    # create the root and the canvas
+    root = Tk()
+    canvas = Canvas(root, width=data.width, height=data.height)
+    canvas.pack()
+    # set up events
+    root.bind("<Button-1>", lambda event:
+                            mousePressedWrapper(event, canvas, data))
+    root.bind("<Key>", lambda event:
+                            keyPressedWrapper(event, canvas, data))
+    timerFiredWrapper(canvas, data)
+    # and launch the app
+    root.mainloop()  # blocks until window is closed
+    print("bye!")
+
+run(1800, 900)
